3c54af53807b8eb4fd398b9db505ff53be436cda
crisMdn <Croow2507@gmail.com>
2026-04-12 12:52:14 -0600

    fix: reorganizar estructura de paquetes y archivos de prueba
    - Mover MinervamindApplication.java a paquete raíz com.minervamind.api
    - Mover DirectConnectionTest.java a src/test/java (ubicación correcta)
    - Esto permite que @SpringBootApplication escanee correctamente todos los componentes

--- src/main/java/com/minervamind/api/MinervamindApplication.java
+++ src/main/java/com/minervamind/api/MinervamindApplication.java
@@ -1,4 +1,4 @@
-package com.minervamind.api.minervamind;
+package com.minervamind.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
